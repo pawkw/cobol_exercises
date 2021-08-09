61e9634791b3f9853436de94cb6fa6988525aa7b
pawkw <pawkw.users.noreply.github.com>
2021-08-09 12:23:00 -0400

    Hello world

---
+++ helloworld.cbl
@@ -0,0 +1,12 @@
+      ******************************************************************
+      * AUTHOR: Peter Weston
+      * Date: 2021-08-09
+      ****************************************************************** 
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. HELLOWORLD.
+       DATA DIVISION. 
+       FILE SECTION. 
+       WORKING-STORAGE SECTION. 
+       PROCEDURE DIVISION. *> Start of program
+           DISPLAY "Hello world.".
+           STOP RUN.
